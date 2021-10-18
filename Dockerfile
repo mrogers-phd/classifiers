4b6234c0818ea13c1fbe21e8236de2584f73caf1
Mark Rogers <mark.rogers@invitae.com>
2021-10-18 15:51:41 -0600

    Create Jupyter notebook within a Docker container

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Docker to generate containers for assembly tests.
+FROM ubuntu
+
+# Eliminate LANG questions
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get -y update && apt-get -y upgrade
+RUN apt-get -y install bash python3-dev python3-pip 
+RUN apt install python-is-python3
+
+RUN pip3 install --upgrade pip
+
+# Enable Jupyter
+RUN pip3 install matplotlib scikit-learn jupyter
+
+# Copy local scripts and modules
+COPY shared/ /home/classifiers/shared/
+COPY bin/ /home/classifiers/bin/
+COPY data/ /home/classifiers/data/
+COPY README.md /home/classifiers/README.md
+
+ENV HOME=/home/classifiers
+ENV PYTHONPATH=.:$HOME/bin:$HOME/shared
+ENV PATH=.:$PATH:$HOME/bin
+ENV JUPYTER_PATH=$PYTHONPATH
+
+WORKDIR $HOME
+
+# Jupyter notebooks
+COPY classifiers.ipynb $HOME
+
+RUN chmod a+rx $HOME/*.*
+
+CMD ["jupyter", "notebook", "--port=8888", "--no-browser", "--ip=0.0.0.0",  "--allow-root"]
---
+++ bin/comparison.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+#
+# Extended from code by Gaël Varoquaux and Andreas Müller
+#   https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+#
+# This script provides a comparison across ten different kinds of classifiers
+# using random two-dimensional data.
+#
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+
+from comparison_utils import *
+
+DESCRIPTION = """
+This script provides a comparison across ten different kinds of classifiers
+using random two-dimensional data.
+
+Possible models are:
+    K = [K]-nearest-neighbors
+    L = [L]inear SVM
+    R = [R]BF SVM
+    P = Gaussian [P]rocess
+    D = [D]ecision tree
+    F = Random [F]orest
+    N = [N]eural network
+    A = [A]daBoost
+    B = Naive [B]ayes
+    Q = [Q]uadratic discriminant
+"""
+
+MODEL_HELP = 'K=k-NN/L=Lin. SVM/R=RBF SVM/P=Gauss./D=D-tree/F=Rand. Forest/N=Neural net/A=AdaBoost/B=Naive Bayes/Q=QDA'
+
+parser = argparse.ArgumentParser(description=DESCRIPTION, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-d', dest='dt_depth', help='Decision tree depth', type=int, default=5)
+parser.add_argument('-e', dest='n_estimators', help='Number of estimators for random forest', type=int, default=10)
+parser.add_argument('-k', help='k-NN parameter', type=int, default=3)
+parser.add_argument('-m', dest='model_codes', help='Models to compare ' + MODEL_HELP, default=MODEL_CODES)
+parser.add_argument('-n', help='Number of samples in dataset', type=int, default=100)
+parser.add_argument('-o', dest='output', help='Render graphics to output file', default=None)
+parser.add_argument('-r', dest='rf_depth', help='Random forest depth', type=int, default=5)
+parser.add_argument('-s', dest='seed', help='Random seed', type=int, default=None)
+parser.add_argument('-G', dest='gamma', help='RBF SVM gamma value', type=float, default=2.0)
+parser.add_argument('-L', dest='linear_c', help='Linear SVM soft margin parameter (C-value)', type=float, default=0.025)
+parser.add_argument('-P', dest='gp_length', help='Gaussian process RBF length parameter', type=float, default=1.0)
+parser.add_argument('-v', help='verbose mode', action='store_true')
+
+args = parser.parse_args()
+
+model_codes = args.model_codes.upper()
+for code in model_codes:
+    if code not in MODEL_CODES:
+        parser.print_help()
+        print('\n** Unrecognized model symbol {}.  Valid codes are {}'.format(code, MODEL_CODES))
+        sys.exit(1)
+
+# Instantiate a model for each classifier type
+selected_models = [MODEL_NAME[c] for c in model_codes]
+classifiers = {}
+for name in selected_models:
+    classifiers[name] = model_factory(name,
+                                      args.k,
+                                      args.linear_c,
+                                      args.gamma,
+                                      args.gp_length,
+                                      args.dt_depth,
+                                      args.rf_depth,
+                                      args.n_estimators)
+
+# Generate three datasets for testing
+datasets = generate_datasets(args.n, args.seed)
+
+# Run the classifiers and render their results
+compare_classifiers(datasets, classifiers, args.seed, args.output, args.v)
